62e59c4d3ff8fc4330c55e804a0cffecf8c22065
maddyking44 <madelineking44@gmail.com>
2018-12-07 18:02:51 +1300

    sumo game v1, one player

---
+++ Assets/Scripts/AddConstantVelocity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour {
+	[SerializeField]
+	Vector3 v3Rolling;
+	
+	// FixedUpdate is called 100 times per second
+	void FixedUpdate () {
+		GetComponent<Rigidbody>().velocity += v3Rolling;
+	}
+}
