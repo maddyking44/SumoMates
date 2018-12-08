09bd2fc620fa60431c4047dd1826be34c1982ac0
maddyking44 <madelineking44@gmail.com>
2018-12-08 17:37:07 +1300

    course and background and winning text trigger done

---
+++ Assets/Scripts/TriggerWin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWin : MonoBehaviour {
+		[SerializeField]
+		GameObject GUITexture; 
+
+		[SerializeField]
+		Vector3 Position;
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("entered trigger");	                             
+		if(other.gameObject.tag == "SpherePlayer")
+		{                                        
+		Instantiate(GUITexture, new Vector3(-2, 5, 34), Quaternion.identity);      
+		Destroy(gameObject);
+		}
+	}
+	void OnTriggerStay (Collider other)
+	{	
+		Debug.Log ("in trigger");
+	}
+	void OnTriggerExit (Collider other)
+	{
+		Debug.Log ("exited trigger");
+	}
+}
